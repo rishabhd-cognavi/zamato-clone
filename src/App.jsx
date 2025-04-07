72b0d3fd1404fd3873ab8edbd1f2973e5e238d03
Rishabh Kumar Das <51358252+rishabhd-cognavi@users.noreply.github.com>
2025-04-07 18:47:40 +0530

    add toast provider to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { Router } from "react-router-dom";
-import { Navigation } from "./components";
 import { AppRouter } from "./components/Router/AppRouter";
+import { Toaster } from "react-hot-toast";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
+      <Toaster
+        position="bottom-right"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 1000,
+          style: {
+            background: "#333",
+            color: "#fff",
+            fontSize: "1rem",
+          },
+        }}
+      />
       <AppRouter />
     </div>
   );
